874122561935240d76c1ceedcd9c66a6b68d082f
awan-jpg <ftwoo.only@gmail.com>
2026-09-21 21:56:15 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  trailingSlash: true,
+  distDir: '.next'
 }
 
 module.exports = nextConfig
